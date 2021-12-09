49c1e3d53bdf9922feea3598c6e7c5dffd4dc2b8
youthanh <youchithanh1@gmail.com>
2021-12-09 21:17:34 +0700

    add getCreditSpent
    thêm phương thức xem số lần còn lại có thể gọi API

--- src/Server/Compiler.java
+++ src/Server/Compiler.java
@@ -10,15 +10,14 @@ import static Services.StringUtils.convertEscapeCharacters;
 public class Compiler {
     private HttpURLConnection JdoodleConnection;
     private String request, console, statusCode, memory, cpuTime;
-
+    //API: https://docs.jdoodle.com/compiler-api/compiler-api
+    private static String clientId = "e1dc961e8571a5bd10efc68f1fa66d48"; //Đăng nhập rồi lấy ở https://www.jdoodle.com/compiler-api/
+    private static String clientSecret = "99d6af4497da794a0e1e1fe5dd45e9cedb80487feb9da835c5e6153c9d9463a5"; //Đăng nhập rồi lấy ở https://www.jdoodle.com/compiler-api/
     /**
      * @param stdin Truyền sẵn dữ liệu đầu vào
      * @param language Truyền đúng cú pháp: java, python3, cpp, php, c
      */
     public Compiler (String script, String stdin, String language) {
-        //API: https://docs.jdoodle.com/compiler-api/compiler-api
-        String clientId = "e1dc961e8571a5bd10efc68f1fa66d48"; //Đăng nhập rồi lấy ở https://www.jdoodle.com/compiler-api/
-        String clientSecret = "99d6af4497da794a0e1e1fe5dd45e9cedb80487feb9da835c5e6153c9d9463a5"; //Đăng nhập rồi lấy ở https://www.jdoodle.com/compiler-api/
         String versionIndex = getVersionIndex(language);
         script = convertEscapeCharacters(script, language);
         stdin = convertEscapeCharacters(stdin, language);
@@ -38,6 +37,11 @@ public class Compiler {
                     + "\",\"versionIndex\":\"" + versionIndex + "\"} ";
         } catch (Exception ignored) {}
     }
+
+    /**
+     * Để gọi getCreditSpent
+     */
+    public Compiler() {}
 
     /**
      * @return Console
@@ -94,6 +98,37 @@ public class Compiler {
         return statusCode;
     }
 
+    /**
+     * @return Số lần còn lại có thể gọi API
+     */
+    public String getCreditSpent() {
+        String used = "";
+        try {
+            URL url = new URL("https://api.jdoodle.com/v1/credit-spent");
+            JdoodleConnection = (HttpURLConnection) url.openConnection();
+            JdoodleConnection.setDoOutput(true);
+            JdoodleConnection.setRequestMethod("POST");
+            JdoodleConnection.setRequestProperty("Content-Type", "application/json");
+
+            request = "{\"clientId\": \"" + clientId
+                    + "\",\"clientSecret\":\"" + clientSecret + "\"} ";
+
+            OutputStream outputStream = JdoodleConnection.getOutputStream();
+            outputStream.write(request.getBytes());
+            outputStream.flush();
+
+            BufferedReader bf_InputStream = new BufferedReader(
+                    new InputStreamReader((JdoodleConnection.getInputStream())));
+            String response;
+            if ((response = bf_InputStream.readLine()) != null) {
+                JSONObject jsonObject = new JSONObject(response);
+                used = jsonObject.get("used").toString();
+            }
+
+        } catch (Exception ignored) {}
+        return used;
+    }
+
     private String getVersionIndex (String language) {
         switch (language) {
             case "java":
@@ -109,52 +144,54 @@ public class Compiler {
     }
 
     // My test ----------------------------------------------------------------------------------------------------------
-    /*public static String readFile(String filePatch) throws IOException{
-        File file = new File(filePatch);
-        Scanner scanner = new Scanner(file);
-        String script = "";
-        while (scanner.hasNextLine()) {
-            script += scanner.nextLine() + "\n";
-        }
-        return script;
-    }
-
+//    public static String readFile(String filePatch) throws IOException{
+//        File file = new File(filePatch);
+//        Scanner scanner = new Scanner(file);
+//        String script = "";
+//        while (scanner.hasNextLine()) {
+//            script += scanner.nextLine() + "\n";
+//        }
+//        return script;
+//    }
+//
     public static void main(String[] args) throws IOException{
         Compiler compiler;
+        compiler = new Compiler();
+        System.out.println(compiler.getCreditSpent());
 
-        System.out.println("java");
-        compiler = new Compiler(
-                readFile("scriptFIles\\sum.java"), "", "java");
-        System.out.println(compiler.compile());
-        System.out.println("cpuTime: " + compiler.getCpuTime());
-        System.out.println("memory: " + compiler.getMemory());
-
-        System.out.println("\npython");
-        compiler = new Compiler(
-                readFile("scriptFIles\\sum.py"), "", "python3");
-        System.out.println(compiler.compile());
-        System.out.println("cpuTime: " + compiler.getCpuTime());
-        System.out.println("memory: " + compiler.getMemory());
-
-        System.out.println("\nC++");
-        compiler = new Compiler(
-                readFile("scriptFIles\\sum.cpp"), "", "cpp");
-        System.out.println(compiler.compile());
-        System.out.println("cpuTime: " + compiler.getCpuTime());
-        System.out.println("memory: " + compiler.getMemory());
-
-        System.out.println("\nC");
-        compiler = new Compiler(
-                readFile("scriptFiles\\sum.c"), "", "c");
-        System.out.println(compiler.compile());
-        System.out.println("cpuTime: " + compiler.getCpuTime());
-        System.out.println("memory: " + compiler.getMemory());
-
-        System.out.println("\nphp");
-        compiler = new Compiler(
-                "", "", "php");
-        System.out.println(compiler.compile());
-        System.out.println("cpuTime: " + compiler.getCpuTime());
-        System.out.println("memory: " + compiler.getMemory());
-    }*/
+//        System.out.println("java");
+//        compiler = new Compiler(
+//                readFile("scriptFIles\\sum.java"), "", "java");
+//        System.out.println(compiler.compile());
+//        System.out.println("cpuTime: " + compiler.getCpuTime());
+//        System.out.println("memory: " + compiler.getMemory());
+//
+//        System.out.println("\npython");
+//        compiler = new Compiler(
+//                readFile("scriptFIles\\sum.py"), "", "python3");
+//        System.out.println(compiler.compile());
+//        System.out.println("cpuTime: " + compiler.getCpuTime());
+//        System.out.println("memory: " + compiler.getMemory());
+//
+//        System.out.println("\nC++");
+//        compiler = new Compiler(
+//                readFile("scriptFIles\\sum.cpp"), "", "cpp");
+//        System.out.println(compiler.compile());
+//        System.out.println("cpuTime: " + compiler.getCpuTime());
+//        System.out.println("memory: " + compiler.getMemory());
+//
+//        System.out.println("\nC");
+//        compiler = new Compiler(
+//                readFile("scriptFiles\\sum.c"), "", "c");
+//        System.out.println(compiler.compile());
+//        System.out.println("cpuTime: " + compiler.getCpuTime());
+//        System.out.println("memory: " + compiler.getMemory());
+//
+//        System.out.println("\nphp");
+//        compiler = new Compiler(
+//                "", "", "php");
+//        System.out.println(compiler.compile());
+//        System.out.println("cpuTime: " + compiler.getCpuTime());
+//        System.out.println("memory: " + compiler.getMemory());
+    }
 }
