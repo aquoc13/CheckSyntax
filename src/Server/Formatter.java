71ac07571311688d449d3119e6b9fa62015ee03a
youthanh <youchithanh1@gmail.com>
2021-12-10 17:45:20 +0700

    update Formatter

--- src/Server/Formatter.java
+++ src/Server/Formatter.java
@@ -3,49 +3,77 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 
+import java.io.File;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Formatter {
     final String URL = "https://www.tutorialspoint.com/online_java_formatter.htm";
+    final String filePath;
 
-    public Formatter (String script) {
+    public Formatter (String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String format() {
+        String formated = "";
         String javaScriptCode;
         Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         final WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setCssEnabled(false);
         try {
             final HtmlPage page = webClient.getPage(URL);
-            // Truyền script lên tutorialspoint server
-//            javaScriptCode = "editor.setValue(" + script + ")";
-//            page.executeJavaScript(javaScriptCode);
 
-            //HtmlElement btn_Beautify = page.getHtmlElementById("beautify");
-            //btn_Beautify.click();
+            // Bốc lấy form uploadFile
+            Thread.sleep(4000); // Chờ page tải xong toàn bộ trang
+            final HtmlFileInput fileInput = page.getHtmlElementById("uploadFile");
+            File scriptFile = new File(filePath);
+            fileInput.setFiles(scriptFile);
 
-            // Để chờ page load xong javaScript
-            //System.out.println("Khoan, dừng khoảng chừng là 2s");
-            Thread.sleep(1000);
+            //Xem đoạn script đã tải lên
+            Thread.sleep(3000); // Chờ page load xong file
+            javaScriptCode = "editor.getValue()";
+            page.executeJavaScript(javaScriptCode).getJavaScriptResult().toString();
+            //System.out.println(formated);
+
+
+            //Click nút format
+            HtmlElement btn_Beautify = page.getHtmlElementById("beautify");
+            btn_Beautify.click();
+
+            //Lấy script formated
+            Thread.sleep(4000); // Chờ page thực thi format xong
             javaScriptCode = "outputeditor.getValue()";
-            String formated = "";
-            while (true) {
-                formated = page.executeJavaScript(javaScriptCode).getJavaScriptResult().toString();
-                if (formated.equals("\n")) {
-                    //System.out.println("1");
-                    Thread.sleep(200);
-                }
-                else
-                    break;
-            }
-            System.out.println(formated);
+            formated = page.executeJavaScript(javaScriptCode).getJavaScriptResult().toString();
+            //System.out.println(formated);
         } catch (Exception ignore) {
             ignore.printStackTrace();
         }
+        return formated;
     }
 
     public static void main (String[] args) {
-        //long start = System.currentTimeMillis();
-        Formatter formatter = new Formatter("hello");
-        //System.out.println(System.currentTimeMillis() - start);
+//        long start = System.currentTimeMillis();
+//        Formatter formatter = new Formatter("demoFiles/helloName.java");
+//        System.out.println(System.currentTimeMillis() - start);
+
+        // testcase
+        Formatter formatter = new Formatter("demoFiles/helloName.java");
+        StringTokenizer string;
+        String temp;
+        for (int i=1; i<=100; i++) {
+            long start = System.currentTimeMillis();
+            string = new StringTokenizer(formatter.format(), "\n");
+            temp = string.nextToken();
+            temp = temp.substring(0, temp.length()-1);
+            System.out.println(temp);
+            if (!temp.equals("import java.io.*;")) {
+                System.out.println("false");
+                break;
+            }
+            System.out.println(i + " " + (System.currentTimeMillis()-start));
+        }
+        System.out.println("Done");
     }
 }
