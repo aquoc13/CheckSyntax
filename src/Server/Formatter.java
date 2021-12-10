b56c157fe3ecb48a6f00073ff9f8ecf532beb1ae
youthanh <youchithanh1@gmail.com>
2021-12-10 14:41:39 +0700

    Update Formatter

--- src/Server/Formatter.java
+++ src/Server/Formatter.java
@@ -1,26 +1,51 @@
 package Server;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Formatter {
-    final String URL = "https://techiedelight.com/tools/java";
-    public Formatter () {
+    final String URL = "https://www.tutorialspoint.com/online_java_formatter.htm";
+
+    public Formatter (String script) {
+        String javaScriptCode;
+        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setCssEnabled(false);
         try {
-            final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-            webClient.getOptions().setJavaScriptEnabled(false);
-            webClient.getOptions().setCssEnabled(false);
             final HtmlPage page = webClient.getPage(URL);
-            //System.out.println(page.getTitleText());
-            final HtmlTextArea textArea = (HtmlTextArea) page.getFirstByXPath("/html/body/div[2]/div/table/tbody/tr[1]/td[1]/div/div/div/div[2]/div[1]/div/textarea");
-            textArea.setText("Hello");
-            System.out.println(textArea.getText());
-        } catch (Exception ignore) {}
+            // Truyền script lên tutorialspoint server
+//            javaScriptCode = "editor.setValue(" + script + ")";
+//            page.executeJavaScript(javaScriptCode);
+
+            //HtmlElement btn_Beautify = page.getHtmlElementById("beautify");
+            //btn_Beautify.click();
+
+            // Để chờ page load xong javaScript
+            //System.out.println("Khoan, dừng khoảng chừng là 2s");
+            Thread.sleep(1000);
+            javaScriptCode = "outputeditor.getValue()";
+            String formated = "";
+            while (true) {
+                formated = page.executeJavaScript(javaScriptCode).getJavaScriptResult().toString();
+                if (formated.equals("\n")) {
+                    //System.out.println("1");
+                    Thread.sleep(200);
+                }
+                else
+                    break;
+            }
+            System.out.println(formated);
+        } catch (Exception ignore) {
+            ignore.printStackTrace();
+        }
     }
 
     public static void main (String[] args) {
-        Formatter formatter = new Formatter();
+        //long start = System.currentTimeMillis();
+        Formatter formatter = new Formatter("hello");
+        //System.out.println(System.currentTimeMillis() - start);
     }
 }
